4beadd7addcb2513e8843a96a4f55de69388bfdd
vetrov78 <vettrov78@gmail.com>
2021-03-14 15:21:12 +0300

    Добавил анимацию для формы запроса (НЕ РАБОТАЕТ)

--- js/search.js
+++ js/search.js
@@ -2,7 +2,10 @@ const button = document.querySelector('.top-search-button');
 const form = document.querySelector('.bottom-search-form');
 
 button.addEventListener('click', function(evt){
+    // debugger;
     evt.preventDefault();
-    button.classList.add('display-none');
+    form.classList.remove('display-none');
+    form.classList.add('form-animation');
 });
 
+console.log(button);
